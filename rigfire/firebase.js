1cef255540c0562ac5b902365c00712ce0e18498
Santhosh <csesanthosh15@gmail.com>
2018-03-13 21:46:10 +0530

    add rigfire folder which has documentation login template

---
+++ rigfire/firebase.js
@@ -0,0 +1,17 @@
+var app_firebase = {};
+(function () {
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyACdVp1O_BwDifqb3CPoQk0Rktfsp5vY0g",
+            authDomain: "rigvehicle.firebaseapp.com",
+            databaseURL: "https://rigvehicle.firebaseio.com",
+            projectId: "rigvehicle",
+            storageBucket: "rigvehicle.appspot.com",
+            messagingSenderId: "432999432639"
+        };
+        firebase.initializeApp(config);
+        app_firebase = firebase;
+    }
+
+
+)
