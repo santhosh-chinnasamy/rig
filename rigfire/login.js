2573a1c6fa241b5ae4bbfd6acb3b3005e3066e7c
avengers <csesanthosh15@gmail.com>
2018-03-14 16:08:01 +0000

    bug fix in firebase.js => missed() and login.js

--- rigfire/login.js
+++ rigfire/login.js
@@ -1,4 +1,5 @@
-(function () {
+(function()
+{
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
     var uiConfig = {
@@ -20,12 +21,13 @@
         signInSuccessUrl: 'main.html',
         signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+
             /*firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             firebase.auth.FacebookAuthProvider.PROVIDER_ID,
             firebase.auth.TwitterAuthProvider.PROVIDER_ID,
             firebase.auth.GithubAuthProvider.PROVIDER_ID,
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,*/
-            firebase.auth.PhoneAuthProvider.PROVIDER_ID
+            firebase.auth.PhoneAuthProvider.PROVIDER_ID*/
         ],
         // Terms of service url.
         tosUrl: 'main.html'
